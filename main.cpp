8898ea3caff9da7f6e62798cead225cd0c7e71f9
Dragos Turmac <>
2020-09-10 11:31:06 +0300

    migrate libjpg, libraw

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSS_OSX
+//
+//  Created by Dragos Turmac on 10/09/2020.
+//  Copyright © 2020 Dragos Turmac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
